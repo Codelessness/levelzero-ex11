4ca5e774d8d98ef2b882bd7b49b90251e2055de9
Ashutosh Aman <ashutoshsingh161216@gmail.com>
2021-12-12 18:41:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// ex11: print every item in list with for loop
+
+/**
+ * prepare a grocerylist with atleat 5 item
+ * print the first item on the list
+ * print the third item on the list
+ * print the last itrm on the list
+ */
+
+var groceryList = ["dudh","bread", "sabji","daba","phool", "agarbatti"];
+
+for (var i=0; i< groceryList.length; i++){
+  console.log(groceryList[i]);
+}
+
